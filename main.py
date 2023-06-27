642e3406343079caeaaa822b3402a29b51f3453e
AneS279 <anes.2017@alunos.utfpr.edu.br>
2023-06-27 20:35:37 -0300

    feat: 100% score on tester

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from flask import request
 app = Flask(__name__)
 
 accounts = {}
-@app.get("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.get("/balance")
+def getBalance():
+    account_id = request.args.get('account_id', '')
+    if(account_id in accounts):
+        return str(accounts[account_id]['amount'])
+    else:
+        return '0', 404
 
 @app.post("/reset")
 def reset():
@@ -27,7 +31,18 @@ def event():
             accounts[bodyRequest['origin']]['amount'] -= int(bodyRequest['amount'])
             return {"origin": {"id": bodyRequest['origin'], 'balance': accounts[bodyRequest['origin']]['amount']}}, 201
         else:
-            return 404
+            return '0', 404
     elif(bodyRequest['type'] == 'transfer'):
-        
+        if(bodyRequest['origin'] in accounts):
+            if(bodyRequest['destination'] in accounts):
+                accounts[bodyRequest['origin']]['amount'] -= int(bodyRequest['amount'])
+                accounts[bodyRequest['destination']]['amount'] += int(bodyRequest['amount'])
+            else:
+                accounts[bodyRequest['destination']] = {'amount': int(bodyRequest['amount'])}
+                accounts[bodyRequest['origin']]['amount'] -= int(bodyRequest['amount'])
+            return {"destination": {"id": bodyRequest['destination'], 'balance': accounts[bodyRequest['destination']]['amount']},
+                    "origin": {"id": bodyRequest['origin'], 'balance': accounts[bodyRequest['origin']]['amount']}}, 201
+        else:
+            return '0', 404
+
 #reset - POST
