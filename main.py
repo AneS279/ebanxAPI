8e25386e22b2be9678531746785b7a268cf96cfb
AneS279 <anes.2017@alunos.utfpr.edu.br>
2023-06-27 20:20:44 -0300

    feat: add event endpoint

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
+accounts = {}
+@app.get("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/reset", methods=['POST'])
+@app.post("/reset")
+def reset():
+    accounts = {}
+    return "OK", 200
 
+@app.post("/event")
+def event():
+    bodyRequest = request.get_json()
+    if(bodyRequest['type'] == 'deposit'):
+        if(bodyRequest['destination'] in accounts):
+            accounts[bodyRequest['destination']]['amount'] += int(bodyRequest['amount'])
+        else:
+            accounts[bodyRequest['destination']] = {'amount': int(bodyRequest['amount'])}
+        return {"destination": {"id": bodyRequest['destination'], 'balance': accounts[bodyRequest['destination']]['amount']}}, 201
+    elif(bodyRequest['type'] == 'withdraw'):
+        if(bodyRequest['origin'] in accounts):
+            accounts[bodyRequest['origin']]['amount'] -= int(bodyRequest['amount'])
+            return {"origin": {"id": bodyRequest['origin'], 'balance': accounts[bodyRequest['origin']]['amount']}}, 201
+        else:
+            return 404
+    elif(bodyRequest['type'] == 'transfer'):
+        
 #reset - POST
